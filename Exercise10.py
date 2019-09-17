a1ad1d6b0d26decd0316444be0fe6d08dbea2e0e
unknown <ben.delisle61@gmail.com>
2019-09-17 14:17:56 -0400

    Corrected to Python3, finished exercise 10

---
+++ Exercise10.py
@@ -0,0 +1,4 @@
+miles = input("How many miles have you drive? ")
+gallon = input("How many gallons of gas have you used? ")
+milesPerGallon = miles / gallon
+print("Your car does", milesPerGallon, "miles per gallon.")
