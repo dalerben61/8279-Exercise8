22e9a9ffe34de2e047ff7c2d89214bbd4ce6dd42
unknown <ben.delisle61@gmail.com>
2019-09-17 14:19:58 -0400

    Corrected syntax in 8

--- Exercise8.py
+++ Exercise8.py
@@ -2,4 +2,4 @@ import math
 pi = math.pi
 radius = input("What is the radius of the circle? ")
 area = pi * radius ** 2
-print ("The area of the circle is", area)
+print("The area of the circle is", area)
