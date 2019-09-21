334b2df0ee371e6d803afa68eac4b9f4451ee8da
unknown <ben.delisle61@gmail.com>
2019-09-21 16:09:36 -0400

    Corrected the user input lines

--- Exercise10.py
+++ Exercise10.py
@@ -1,4 +1,4 @@
-miles = input("How many miles have you drive? ")
-gallon = input("How many gallons of gas have you used? ")
+miles = int(input("How many miles have you drive? "))
+gallon = int(input("How many gallons of gas have you used? "))
 milesPerGallon = miles / gallon
 print("Your car does", milesPerGallon, "miles per gallon.")
--- Exercise8.py
+++ Exercise8.py
@@ -1,5 +1,5 @@
 import math
 pi = math.pi
-radius = input("What is the radius of the circle? ")
+radius = int(input("What is the radius of the circle? "))
 area = pi * radius ** 2
 print("The area of the circle is", area)
